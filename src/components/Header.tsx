3d5f9fe5cc8dd23d7fe0379af4fc42a1a40ed38d
Henrique Oliveira <oliveira@MacBook-Pro-de-Henrique.local>
2025-05-15 12:04:36 -0300

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header className="bg-white container mx-auto">
+      <nav className="flex items-center justify-between mx-auto px-4 mt-5 mb-10" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
             <Image className="h-8 w-auto" src="/images/logo.png" width={200} height={100} alt="" />
